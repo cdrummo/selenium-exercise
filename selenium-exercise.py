6aed24a7998bfcd0f4a2156a102335f0ad346963
Connor Drummond <32054606+cdrummo@users.noreply.github.com>
2024-08-01 21:15:05 -0600

    Refactor of code
    Refactored code to remove inconsistencies and improve readability. Reworked variables to reduce unnecessary storage and improve runtimes.

--- selenium-exercise.py
+++ selenium-exercise.py
@@ -1,10 +1,9 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -12,55 +11,31 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("http://sdetchallenge.fetch.com/")
 
-i = 0
-while i < 3:
+for i in range(3):
     driver.find_element("id", "left_" + str(i)).send_keys(str(i))
     driver.find_element("id", "right_" + str(i)).send_keys(str(3 + i))
-    i += 1
 
 driver.find_element("id", "weigh").click()
-
 time.sleep(3)
+result = driver.find_element("id", "reset").text
 
-result_1 = driver.find_element(By.ID, "reset").text
 
-j = 1
-while j < 3:
-    driver.find_element("id", "left_" + str(j)).send_keys(Keys.CONTROL + "a")
-    driver.find_element("id", "left_" + str(j)).send_keys(Keys.DELETE)
-    driver.find_element("id", "right_" + str(j)).send_keys(Keys.CONTROL + "a")
-    driver.find_element("id", "right_" + str(j)).send_keys(Keys.DELETE)
-    j += 1
+for i in range(1,3):
+    driver.find_element("id", "left_" + str(i)).send_keys(Keys.CONTROL + "a")
+    driver.find_element("id", "left_" + str(i)).send_keys(Keys.DELETE)
+    driver.find_element("id", "right_" + str(i)).send_keys(Keys.CONTROL + "a")
+    driver.find_element("id", "right_" + str(i)).send_keys(Keys.DELETE)
 
-right = 1 if result_1 == "<" else 4 if result_1 == ">" else 7
-left = 0 if result_1 == "<" else 3 if result_1 == ">" else 6
+answer = 0 if result == "<" else 3 if result == ">" else 6
 
 driver.find_element("id", "left_0" ).send_keys(Keys.CONTROL + "a")
-driver.find_element("id", "left_0").send_keys(str(left))
+driver.find_element("id", "left_0").send_keys(str(answer))
 driver.find_element("id", "right_0").send_keys(Keys.CONTROL + "a")
-driver.find_element("id", "right_0").send_keys(str(right))
+driver.find_element("id", "right_0").send_keys(str(answer + 1))
 
 driver.find_element("id", "weigh").click()
-
 time.sleep(3)
+result = driver.find_element("id", "reset").text
 
-result_2 = driver.find_element(By.ID, "reset").text
-
-if result_1 == "<" and result_2 == "<":
-    driver.find_element("id", "coin_0").click()
-elif result_1 == "<" and result_2 == ">":
-    driver.find_element("id", "coin_1").click()
-elif result_1 == "<" and result_2 == "=":
-    driver.find_element("id", "coin_2").click()
-elif result_1 == ">" and result_2 == "<":
-    driver.find_element("id", "coin_3").click()
-elif result_1 == ">" and result_2 == ">":
-    driver.find_element("id", "coin_4").click()
-elif result_1 == ">" and result_2 == "=":
-    driver.find_element("id", "coin_5").click()
-elif result_1 == "=" and result_2 == "<":
-    driver.find_element("id", "coin_6").click()
-elif result_1 == "=" and result_2 == ">":
-    driver.find_element("id", "coin_7").click()
-else:
-    driver.find_element("id", "coin_8").click()
+answer += 0 if result == "<" else 1 if result == ">" else 2
+driver.find_element("id", "coin_" + str(answer)).click()
